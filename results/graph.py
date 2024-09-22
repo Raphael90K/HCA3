f46a7c6d3abe6143a7475a7b96e10ee5e717e749
Raphael <raphael.kropp@web.de>
2024-09-22 20:27:40 +0200

    Results

--- results/graph.py
+++ results/graph.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 
 device = ["i9 CPU", "GF RTX 4070", "RPi CPU", "Hailo8l"]
-avg = [0.0332, 0.0542, 0.1046, 0.0105]
+avg = [0.0332, 0.0542, 0.1387, 0.0105]
 
 fig, ax = plt.subplots()
 
